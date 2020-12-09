d38c8b77e862c3e069a7e22613cb5dc55f325d3a
Jonas Pik <j.pikiotas@gmail.com>
2020-12-09 00:24:33 +0000

    Authentication with Firebase

--- src/App.js
+++ src/App.js
@@ -4,8 +4,35 @@ import Home from './Home';
 import Checkout from './Checkout';
 import Login from './Login';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import React, { useEffect } from "react";
+import { auth } from "./firebase";
+import { useStateValue } from "./StateProvider";
 
 function App() {
+
+  const [{ }, dispatch] = useStateValue();
+
+  useEffect(() => {
+    // Will only run once when app component loads []
+    auth.onAuthStateChanged(authUser => {
+      console.log('USER IS >>>', authUser);
+
+      if (authUser) {
+        // User is logged in
+        dispatch({
+          type: 'SET_USER',
+          user: authUser
+        })
+      } else {
+        // User is logged out
+        dispatch({
+          type: 'SET_USER',
+          user: null
+        })
+      }
+    })
+  }, [])
+
   return (
     /* using BEM convention */
     <Router>
--- src/firebase.js
+++ src/firebase.js
@@ -1,11 +1,20 @@
+import firebase from "firebase";
+
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyDJWZfw8so6Cd6sVJ0bKsp_yOgT9LiatI0",
-    authDomain: "clone-44d6c.firebaseapp.com",
-    databaseURL: "https://clone-44d6c.firebaseio.com",
-    projectId: "clone-44d6c",
-    storageBucket: "clone-44d6c.appspot.com",
-    messagingSenderId: "714693741862",
-    appId: "1:714693741862:web:2d02c0ba60b86f85ab85e7",
-    measurementId: "G-T2E3KX4PPW"
-  };
+  apiKey: "AIzaSyDJWZfw8so6Cd6sVJ0bKsp_yOgT9LiatI0",
+  authDomain: "clone-44d6c.firebaseapp.com",
+  databaseURL: "https://clone-44d6c.firebaseio.com",
+  projectId: "clone-44d6c",
+  storageBucket: "clone-44d6c.appspot.com",
+  messagingSenderId: "714693741862",
+  appId: "1:714693741862:web:2d02c0ba60b86f85ab85e7",
+  measurementId: "G-T2E3KX4PPW"
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { db, auth };
